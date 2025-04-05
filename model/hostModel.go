d748c3df4b0fa8b073e223fd4e010415825a0050
Gary Greene <greeneg@tolharadys.net>
2025-04-05 19:40:48 -0400

    More deadlock fixes
    Signed-off-by: Gary Greene <greeneg@tolharadys.net>

--- model/hostModel.go
+++ model/hostModel.go
@@ -22,87 +22,161 @@ import (
 	"database/sql"
 	"encoding/json"
 	"log"
+	"strconv"
 )
 
 func CreateHost(h Host, id int) (bool, error) {
+	log.Println("INFO: Creating host " + h.HostName)
 	t, err := DB.Begin()
 	if err != nil {
+		log.Println("ERROR: Failed to begin transaction")
 		return false, err
 	}
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("ERROR: Failed to create host " + h.HostName)
+			t.Rollback()
+		}
+		if err != nil {
+			log.Println("ERROR: Failed to create host " + h.HostName)
+			t.Rollback()
+		}
+	}()
 
 	q, err := t.Prepare("INSERT INTO Hosts (HostName, MacAddresses, CreatorId) VALUES (?, ?, ?)")
 	if err != nil {
+		log.Println("ERROR: Failed to prepare statement")
 		return false, err
 	}
 
 	strJsonMacAddressesSlice, err := json.Marshal(h.MacAddresses)
 	if err != nil {
+		log.Println("ERROR: Failed to marshal mac addresses")
 		return false, err
 	}
+
 	_, err = q.Exec(h.HostName, strJsonMacAddressesSlice, id)
 	if err != nil {
+		log.Println("ERROR: Failed to execute statement")
 		return false, err
 	}
 
-	t.Commit()
+	err = t.Commit()
+	if err != nil {
+		log.Println("ERROR: Failed to commit transaction")
+		return false, err
+	}
 
+	log.Println("INFO: Host " + h.HostName + " created successfully")
 	return true, nil
 }
 
 func DeleteHostname(hostname string) (bool, error) {
+	log.Println("INFO: Deleting host " + hostname)
 	t, err := DB.Begin()
 	if err != nil {
+		log.Println("ERROR: Failed to begin transaction")
 		return false, err
 	}
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("ERROR: Failed to delete host " + hostname)
+			t.Rollback()
+		}
+		if err != nil {
+			log.Println("ERROR: Failed to delete host " + hostname)
+			t.Rollback()
+		}
+	}()
 
 	q, err := t.Prepare("DELETE FROM Hosts WHERE HostName = ?")
 	if err != nil {
+		log.Println("ERROR: Failed to prepare statement")
 		return false, err
 	}
 
 	_, err = q.Exec(hostname)
 	if err != nil {
+		log.Println("ERROR: Failed to execute statement")
 		return false, err
 	}
 
-	t.Commit()
+	err = t.Commit()
+	if err != nil {
+		log.Println("ERROR: Failed to commit transaction")
+		return false, err
+	}
 
+	log.Println("INFO: Host " + hostname + " deleted successfully")
 	return true, nil
 }
 
 func UpdateMacAddresses(hostname string, data []string) (bool, error) {
+	log.Println("INFO: Updating MAC addresses for host " + hostname)
 	t, err := DB.Begin()
 	if err != nil {
+		log.Println("ERROR: Failed to begin transaction")
 		return false, err
 	}
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("ERROR: Failed to update MAC addresses for host " + hostname)
+			t.Rollback()
+		}
+		if err != nil {
+			log.Println("ERROR: Failed to update MAC addresses for host " + hostname)
+			t.Rollback()
+		}
+	}()
 
 	q, err := t.Prepare("UPDATE Hosts SET MacAddresses = ? WHERE HostName = ?")
 	if err != nil {
+		log.Println("ERROR: Failed to prepare statement")
 		return false, err
 	}
 
 	macAddressSlice, err := json.Marshal(data)
 	if err != nil {
+		log.Println("ERROR: Failed to marshal mac addresses")
 		return false, err
 	}
+
 	_, err = q.Exec(macAddressSlice, hostname)
 	if err != nil {
+		log.Println("ERROR: Failed to execute statement")
 		return false, err
 	}
 
-	t.Commit()
+	err = t.Commit()
+	if err != nil {
+		log.Println("ERROR: Failed to commit transaction")
+		return false, err
+	}
 
+	log.Println("INFO: MAC addresses for host " + hostname + " updated successfully")
 	return true, nil
 }
 
 func GetHostById(id int) (Host, error) {
+	idStr := strconv.Itoa(id)
+	log.Println("INFO: Getting host by ID: " + idStr)
 	rec, err := DB.Prepare("SELECT * FROM Hosts WHERE Id = ?")
 	if err != nil {
+		log.Println("ERROR: Failed to prepare statement")
 		return Host{}, err
 	}
+	defer rec.Close()
 
 	strHost := StringHost{}
-	err = rec.QueryRow(id).Scan(
+
+	r, err := rec.Query(id)
+	if err != nil {
+		log.Println("ERROR: Failed to execute statement")
+		return Host{}, err
+	}
+	defer r.Close()
+
+	err = r.Scan(
 		&strHost.Id,
 		&strHost.HostName,
 		&strHost.MacAddresses,
@@ -111,8 +185,10 @@ func GetHostById(id int) (Host, error) {
 	)
 	if err != nil {
 		if err == sql.ErrNoRows {
+			log.Println("ERROR: No host found with ID: " + idStr)
 			return Host{}, nil
 		}
+		log.Println("ERROR: Failed to scan rows")
 		return Host{}, err
 	}
 	// process the MAC addresses
@@ -125,17 +201,28 @@ func GetHostById(id int) (Host, error) {
 	host.CreatorId = strHost.CreatorId
 	host.CreationDate = strHost.CreationDate
 
+	log.Println("INFO: Host " + host.HostName + " found")
 	return host, nil
 }
 
 func GetHostByHostName(hostname string) (Host, error) {
+	log.Println("INFO: Getting host by name: " + hostname)
 	rec, err := DB.Prepare("SELECT * FROM Hosts WHERE HostName = ?")
 	if err != nil {
+		log.Println("ERROR: Failed to prepare statement")
 		return Host{}, err
 	}
+	defer rec.Close()
 
 	strHost := StringHost{}
-	err = rec.QueryRow(hostname).Scan(
+	r, err := rec.Query(hostname)
+	if err != nil {
+		log.Println("ERROR: Failed to execute statement")
+		return Host{}, err
+	}
+	defer r.Close()
+
+	err = r.Scan(
 		&strHost.Id,
 		&strHost.HostName,
 		&strHost.MacAddresses,
@@ -144,10 +231,13 @@ func GetHostByHostName(hostname string) (Host, error) {
 	)
 	if err != nil {
 		if err == sql.ErrNoRows {
+			log.Println("ERROR: No host found with name: " + hostname)
 			return Host{}, nil
 		}
+		log.Println("ERROR: Failed to scan rows")
 		return Host{}, err
 	}
+
 	// process the MAC addresses
 	unmarshalledMacAddresses := make([]string, 0)
 	_ = json.Unmarshal([]byte(strHost.MacAddresses), &unmarshalledMacAddresses)
@@ -158,14 +248,18 @@ func GetHostByHostName(hostname string) (Host, error) {
 	host.CreatorId = strHost.CreatorId
 	host.CreationDate = strHost.CreationDate
 
+	log.Println("INFO: Host " + host.HostName + " found")
 	return host, nil
 }
 
 func GetHosts() ([]Host, error) {
+	log.Println("INFO: Getting all hosts")
 	rows, err := DB.Query("SELECT * FROM Hosts")
 	if err != nil {
+		log.Println("ERROR: Failed to execute statement")
 		return nil, err
 	}
+	defer rows.Close()
 
 	hosts := make([]Host, 0)
 	for rows.Next() {
@@ -178,8 +272,14 @@ func GetHosts() ([]Host, error) {
 			&strHost.CreationDate,
 		)
 		if err != nil {
+			if err == sql.ErrNoRows {
+				log.Println("ERROR: No hosts found")
+				return nil, nil
+			}
+			log.Println("ERROR: Failed to scan rows")
 			return nil, err
 		}
+
 		// process json of MacAddresses to remove unneeded escapes
 		unmarshalledMacAddresses := make([]string, 0)
 		_ = json.Unmarshal([]byte(strHost.MacAddresses), &unmarshalledMacAddresses)
@@ -197,5 +297,6 @@ func GetHosts() ([]Host, error) {
 		hosts = append(hosts, host)
 	}
 
+	log.Println("INFO: Found " + strconv.Itoa(len(hosts)) + " hosts")
 	return hosts, nil
 }
